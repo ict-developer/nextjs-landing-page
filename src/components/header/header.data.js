f4b6bbcad3396e044ea27f7f643a84a9b2371ed9
ict-developer <ict-developer@protonmail.com>
2022-07-22 18:47:32 +0700

    publish 2 vercel

---
+++ src/components/header/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: 'home',
+    label: 'Trang chủ',
+  },
+  {
+    path: 'feature',
+    label: 'Dịch vụ',
+  },
+  {
+    path: 'xp',
+    label: 'Kinh nghiệm',
+  },
+  {
+    path: 'product',
+    label: 'Sản phẩm',
+  },
+];
